a2b9917a1073ee4b26cf26221330bc482e0feef6
Jimmy <keepcreate06@gmail.com>
2025-08-30 16:09:21 +0800

    實作project 2025/08/30 16:09

---
+++ src/components/ProductNotification.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="position-fixed top-0 end-0 p-2" style="z-index: 1050">
+    <div class="toast show align-items-center text-white bg-success border-0">
+      <div class="d-flex">
+        <div class="toast-body">這是通知訊息</div>
+        <button type="button" class="btn-close btn-close-white me-2 m-auto"></button>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/TaskOneView.vue
+++ src/views/TaskOneView.vue
@@ -1,5 +1,25 @@
-<template>
-  <h1>進階任務</h1>
-</template>
+<!-- 需求功能描述 -->
+<!-- 請嘗試將「商品列表」、「購物車」、「通知」拆分成 3 個元件 -->
+<!-- 請使用 props 將商品資料傳遞至「商品列表」元件 -->
+<!-- 「購物車」元件的刪除功能，請使用 emit 傳遞事件 -->
+<!-- 使用 provide, inject 完成通知功能 -->
+<script setup>
+import ProductList from '../components/ProductList.vue';
+import ProductCart from '../components/ProductCart.vue';
+import ProductNotification from '../components/ProductNotification.vue';
+</script>
 
-<style></style>
+<template>
+  <div id="app" class="container py-5">
+    <div class="row">
+      <!-- 商品列表區 -->
+      <ProductList />
+
+      <!-- 購物車區 -->
+      <ProductCart />
+    </div>
+
+    <!-- 通知元件 -->
+    <ProductNotification />
+  </div>
+</template>
